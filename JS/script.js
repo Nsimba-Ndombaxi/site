ab2da53ba159fd48526a1151af8cc0e92b0f4271
Nsimba-Ndombaxi <nsimbandombaxi12@gmail.com>
2025-07-21 22:50:50 +0100

    os arquivos do meu site
    o meu portfólio

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function criador(){
+    document.getElementById("criador").src="SRC/Images/image1.jpg";
+}
+
+function logo(){
+    document.getElementById("criador").src="SRC/Images/image13.jpg";
+}
